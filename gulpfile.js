f131bc95fca49b67766ea78443b1c4c99f588b3c
Vykonat <35694895+Vykonat@users.noreply.github.com>
2018-02-01 15:34:00 -0600

    Blog links complete

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,6 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./css'));
 });
 
-gulp.task('watch', function () {
+gulp.task('sass:watch', function () {
   gulp.watch('./scss/**/*.scss', ['sass']);
 });
